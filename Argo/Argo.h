899df4acbf5c80aeb9e30daf8dbbb3a37ec51c10
Steve Brambilla <stevebrambilla@me.com>
2015-09-19 11:35:13 -0400

    Add tvOS target

---
+++ Argo/Argo.h
@@ -0,0 +1,19 @@
+//
+//  Argo.h
+//  Argo
+//
+//  Created by Steve Brambilla on 2015-09-18.
+//  Copyright © 2015 thoughtbot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Argo.
+FOUNDATION_EXPORT double ArgoVersionNumber;
+
+//! Project version string for Argo.
+FOUNDATION_EXPORT const unsigned char ArgoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Argo/PublicHeader.h>
+
+
